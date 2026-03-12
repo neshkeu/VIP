815ce176b3985b8cc1cca595c123af88f6263bcb
neshkeu <76601017+neshkeu@users.noreply.github.com>
2026-03-12 22:12:47 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AppLayout } from "@/components/AppLayout";
+import { AuthGuard } from "@/components/AuthGuard";
+import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import DriversPage from "./pages/DriversPage";
 import DriverDetailPage from "./pages/DriverDetailPage";
@@ -23,20 +25,27 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <AppLayout>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/drivers" element={<DriversPage />} />
-            <Route path="/drivers/:id" element={<DriverDetailPage />} />
-            <Route path="/vehicles" element={<VehiclesPage />} />
-            <Route path="/vehicles/:id" element={<VehicleDetailPage />} />
-            <Route path="/rentals" element={<RentalsPage />} />
-            <Route path="/payments" element={<PaymentsPage />} />
-            <Route path="/expenses" element={<ExpensesPage />} />
-            <Route path="/pos-reports" element={<PosReportsPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AppLayout>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/*" element={
+            <AuthGuard>
+              <AppLayout>
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/drivers" element={<DriversPage />} />
+                  <Route path="/drivers/:id" element={<DriverDetailPage />} />
+                  <Route path="/vehicles" element={<VehiclesPage />} />
+                  <Route path="/vehicles/:id" element={<VehicleDetailPage />} />
+                  <Route path="/rentals" element={<RentalsPage />} />
+                  <Route path="/payments" element={<PaymentsPage />} />
+                  <Route path="/expenses" element={<ExpensesPage />} />
+                  <Route path="/pos-reports" element={<PosReportsPage />} />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </AppLayout>
+            </AuthGuard>
+          } />
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
